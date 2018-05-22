befe7845fd0f2f14a4d9ef4982159b3986c55bc4
Eetu Jääsola <eetujaa>
2018-05-22 15:13:21 +0300

    bim

--- iot/phat.py
+++ iot/phat.py
@@ -8,7 +8,7 @@ import time
 
 def readSensors():
     sensors = {
-        "temperature": envirophat.weather.temperature()
+        "temperature": envirophat.motion.accelerometer()
     }
 
     return sensors 
